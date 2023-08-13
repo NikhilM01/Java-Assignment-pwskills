04cf25946683733c3b0a186ca955fa37e4a4a5b0
Nikhil <nikhilmuneshwar01@gmail.com>
2023-08-13 21:22:55 +0530

    Mutable String

---
+++ String Assignment/Test8.java
@@ -0,0 +1,16 @@
+public class Test8 {
+
+    public static void main(String args[]) {
+
+        StringBuffer s1 = new StringBuffer("Virat");
+        StringBuilder s2 = new StringBuilder("Nikhil");
+
+        s1.append("kohli");
+        s2.append("MUneshwar");
+
+        System.out.println(s1);
+        System.out.println(s2);
+
+    }
+
+}
