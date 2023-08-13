353ebc74e6aa2d5855204c9cc28b163c3bf92d95
Nikhil <nikhilmuneshwar01@gmail.com>
2023-08-13 12:27:39 +0530

    String Concatenation

---
+++ String Assignment/Test2.java
@@ -0,0 +1,22 @@
+public class Test2 {
+    public static void main(String[] args) {
+
+        String s1 = "nikhil";
+        String s2 = " muneshwar";
+        String s3 = new String("nikhil");
+        String s4 = new String(" muneshwar");
+
+        s1=s1.concat(" muneshwar"); 
+        s3=s3.concat(s4);  
+        String s5 = s1 +s2;
+
+        System.out.println(s1);
+        System.out.println(s2);
+        System.out.println(s3);
+        System.out.println(s4);
+        System.out.println(s5  + s1 + s4);
+
+       
+    }
+
+}
