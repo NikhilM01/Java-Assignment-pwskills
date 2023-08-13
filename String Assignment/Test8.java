b596c42fe834b232da0ca6663b4dc7cbaee4d2fb
Nikhil <nikhilmuneshwar01@gmail.com>
2023-08-13 22:04:12 +0530

    Mutable String

--- String Assignment/Test8.java
+++ String Assignment/Test8.java
@@ -11,16 +11,42 @@ public class Test8 {
         System.out.println(s1);
         System.out.println(s2);
 
-
         // Final vs Immutability
-        final int a=10;
-       // a=20;   error
+        final int a = 10;
+        // a=20; error
         System.out.println(a);
-        final StringBuffer sb=new StringBuffer("Virat");
+        final StringBuffer sb = new StringBuffer("Virat");
         sb.append("kohli");
         // System.out.println(sb);
-       // sb=new StringBuffer("Sachin");  error
+        // sb=new StringBuffer("Sachin"); error
         System.out.println(sb);
+
+        //
+        StringBuilder s3 = new StringBuilder();
+        System.out.println(s3.capacity());
+        s3.append("abcdefghijklmnop");
+        System.out.println(s3.capacity());
+        s3.append("s");
+        System.out.println(s3.capacity());  // how many character you can add 16+1 (17*2= 34 size)
+        System.out.println(s3.length());  // how mancy character r present
+
+        StringBuffer s32 = new StringBuffer("Sachin");
+        System.out.println(s32);
+        System.out.println(s32.capacity());
+        System.out.println(s32.charAt(1));
+        s32.setCharAt(1, 'A'); // SAchin
+        System.out.println(s32);
+
+        StringBuilder s4 = new StringBuilder(150);
+        System.out.println(s4.capacity());
+        s4.append("java");
+        System.out.println(s4); // 150
+        s4.trimToSize();
+        System.out.println(s4.capacity()); // 4
+
+        StringBuilder s5 = new StringBuilder("nikhil");
+        System.out.println(s5);
+        System.out.println(s5.reverse()); // lihkin
 
     }
 
